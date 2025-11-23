bd838799c1c717545ce2ee0d73620520c6424d74
13vickyy <mvicky.fern@gmail.com>
2025-11-23 01:42:47 -0300

    Ej1+Ej2

---
+++ Ejercicio/P1.java
@@ -0,0 +1,10 @@
+package Ejercicio; //1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
+
+public class P1 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Mi nombre es Victoria");
+    }
+    
+}
---
+++ Ejercicio/P2.java
@@ -0,0 +1,10 @@
+package Ejercicio; //2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
+
+public class P2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola\nMundo");
+    }
+    
+}
