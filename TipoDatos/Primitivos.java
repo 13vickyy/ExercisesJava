56daa67c54de8526663620cbe4b8f45c855050e5
13vickyy <mvicky.fern@gmail.com>
2025-11-24 21:34:39 -0300

    TipoDatos + EjercicioVariables

---
+++ TipoDatos/Primitivos.java
@@ -0,0 +1,23 @@
+package TipoDatos;
+
+public class Primitivos {
+    public static void main(String[] args) {
+        
+        int MyInt = 29;
+        System.out.println(MyInt);
+
+        double MyDouble = 3.14; //Estandar de "float" en JAVA
+        System.out.println(MyDouble);
+
+        char MyChar = 'A'; //Comillas SIMPLES. Dobles es para string
+        System.out.println(MyChar);
+        
+        boolean MyBoolean = false;
+        System.out.println(MyBoolean);
+
+        String MyString = "Esto no es un tipo de dato primitivo, en realidad es una clase, pero se utiliza mucho";
+        System.out.println(MyString);
+
+    }
+    
+}
