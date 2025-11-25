4f2a175293713e7123a375033adfb4a3f561019c
13vickyy <mvicky.fern@gmail.com>
2025-11-24 21:41:22 -0300

    p1 variable

---
+++ VariablesYconstantes/p1.java
@@ -0,0 +1,11 @@
+package VariablesYconstantes; // 1. Declara una variable de tipo String y asígnale tu nombre.
+
+public class p1 {
+    public static void main(String[] args) {
+        
+        String name = "Victoria";
+        System.out.println(name);
+
+    }
+    
+}
