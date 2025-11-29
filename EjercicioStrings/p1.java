d123874c55ee54dcdfa1d9e2541c70e9c2653313
13vickyy <mvicky.fern@gmail.com>
2025-11-29 20:56:39 -0300

    p1 strings

---
+++ EjercicioStrings/p1.java
@@ -0,0 +1,12 @@
+package EjercicioStrings; // 1. Concatena dos cadenas de texto.
+
+public class p1 {
+    public static void main(String[] args) {
+        var palabra_1 = "Hola";
+        var palabra_2 = "Como estas";
+
+        var resultado = palabra_1 + " " + palabra_2;
+        System.out.println(resultado);
+    }
+    
+}
