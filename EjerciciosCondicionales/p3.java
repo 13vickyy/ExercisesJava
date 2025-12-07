24aeb710556d38b1dc539cf4a3e611f418981dcd
13vickyy <mvicky.fern@gmail.com>
2025-12-07 15:55:34 -0300

    p3 EjerciciosCondicionales

---
+++ EjerciciosCondicionales/p3.java
@@ -0,0 +1,20 @@
+public class p3 { // 3. Dado un número, verifica si es positivo, negativo o cero.
+    public static void main(String[] args) {
+        int nro = 0;
+
+   if (nro == 0) {
+
+            System.out.println("El nro es igual a 0");
+        } 
+        
+        else if (nro > 0) {
+
+            System.out.println("El nro es positivo");
+        } 
+        
+        else {
+            System.out.println("El nro es negativo");
+        }
+    }
+    
+}
