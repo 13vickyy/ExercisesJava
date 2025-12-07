8938c943b2cd560f3f77c0e9847abef5954fddd3
13vickyy <mvicky.fern@gmail.com>
2025-12-07 15:51:24 -0300

    p1-p2 EjerciciosCondicionales

---
+++ EjerciciosCondicionales/p1.java
@@ -0,0 +1,12 @@
+// 1. Establece la edad de un usuario y muestra si puede votar (mayor o igual a 18).
+public class p1 { 
+    public static void main(String[] args) {
+        
+        int age = 18;
+
+        if (age >= 18){
+            System.out.println("El usuario es mayor de edad, puede votar");
+        }
+    }
+    
+}
---
+++ EjerciciosCondicionales/p2.java
@@ -0,0 +1,18 @@
+public class p2 { // 2. Declara dos números y muestra cuál es mayor, o si son iguales.
+
+    public static void main(String[] args) {
+        int numero_1 = 13;
+        int numero_2 = 29;
+
+        if(numero_1 == numero_2){
+            System.out.println("Nro 1 es igual a Nro 2");
+        }
+            else if (numero_1 < numero_2){
+                System.out.println("Nro 2 es mayor que Nro 1");
+            }
+                else {
+                    System.out.println("Nro 1 es mayor que Nro 2");
+                }
+    }
+    
+}
